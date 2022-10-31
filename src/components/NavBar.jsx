5cc7b5eb280671c0a8369b1fa360426dc7280fcd
Asti <astistateofmind@gmail.com>
2022-10-31 13:47:07 -0700

    add drop down menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,13 @@ import React from "react";
 export default function NavBar() {
   return (
     <>
-      <div className="lines-wrap">
-        <div className="lines-inner">
-          <div className="lines"></div>
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close">
+            <span class="icofont-close js-menu-toggle"></span>
+          </div>
         </div>
+        <div class="site-mobile-menu-body"></div>
       </div>
       <nav className="site-nav">
         <div className="container">
@@ -15,7 +18,26 @@ export default function NavBar() {
               Art Window
             </a>
 
-            <ul className="js-clone-nav d-none d-lg-inline-noone text-left site-menu"></ul>
+            <ul className="js-clone-nav d-none d-lg-inline-noone text-left site-menu">
+              <li class="active">
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+              </li>
+              <li>
+                <a href="">Portfolio</a>
+              </li>
+              <li>
+                <a href="">Elements</a>
+              </li>
+              <li>
+                <a href="">About</a>
+              </li>
+              <li>
+                <a href="">Contact Us</a>
+              </li>
+            </ul>
 
             <a
               href=""
