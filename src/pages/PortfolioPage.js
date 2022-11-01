b3e6263fe44738e6f7ee4d2650bf93dd40e953c5
Asti <astistateofmind@gmail.com>
2022-11-01 14:56:26 -0700

    add galery component

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Gallery from '../components/Gallery';
 
 export default function PortfolioPage() {
   return (
+    <>
     <div>PortfolioPage</div>
+      <Gallery />
+      </>
   )
 }
