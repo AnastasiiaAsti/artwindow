76640c53807b9dd7b963f6e6f95e6f8271b466ed
Asti <astistateofmind@gmail.com>
2022-11-02 18:54:24 -0700

    correct misspelings

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export default function Gallery() {
   return (
     <>
-      <div class="site-mobile-menu site-navbar-target">
-        <div class="site-mobile-menu-header">
-          <div class="site-mobile-menu-close">
-            <span class="icofont-close js-menu-toggle"></span>
+      <div className="site-mobile-menu site-navbar-target">
+        <div className="site-mobile-menu-header">
+          <div className="site-mobile-menu-close">
+            <span className="icofont-close js-menu-toggle"></span>
           </div>
         </div>
-        <div class="site-mobile-menu-body"></div>
+        <div className="site-mobile-menu-body"></div>
       </div>
 
-      <div class="site-section" id="portfolio-section">
-        <div class="container">
+      <div className="site-section" id="portfolio-section">
+        <div className="container">
           <div className="custom-hero">
             <div className="container">
               <div className="row justify-content-center">
@@ -30,15 +30,15 @@ export default function Gallery() {
             </div>
           </div>
 
-          <div class="site-section" id="portfolio-section">
-            <div class="container">
+          <div className="site-section" id="portfolio-section">
+            <div className="container">
               <div
-                class="filters-content mb-5"
+                className="filters-content mb-5"
                 data-aos="fade-up"
                 data-aos-delay="200"
               >
-                <div class="row grid">
-                  <div class="isotope-card col-sm-4 all mockup">
+                <div className="row grid">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666946943661-fc8e773eadc3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8TThqVmJMYlRSd3N8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -46,16 +46,16 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666946943661-fc8e773eadc3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8TThqVmJMYlRSd3N8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
 
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666715576714-c6ec406f2746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -63,16 +63,16 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666715576714-c6ec406f2746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
 
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1659991147507-743a51706cf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -80,15 +80,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1659991147507-743a51706cf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1661734421319-8c3b7a721118?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -96,15 +96,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1661734421319-8c3b7a721118?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=600"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666807849815-5fd29a8d68a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -112,15 +112,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666807849815-5fd29a8d68a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666742871765-4847beb1a26e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -128,15 +128,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666742871765-4847beb1a26e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666207652513-54b2f94b9b07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYwfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -144,15 +144,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666207652513-54b2f94b9b07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYwfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666713725275-1563c5ab8fe0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -160,15 +160,15 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666713725275-1563c5ab8fe0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYxfE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
-                  <div class="isotope-card col-sm-4 all mockup">
+                  <div className="isotope-card col-sm-4 all mockup">
                     <a
                       href="https://images.unsplash.com/photo-1666205450682-2c78a4929a5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDg1fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                       data-fancybox="gal"
@@ -176,11 +176,11 @@ export default function Gallery() {
                       <img
                         src="https://images.unsplash.com/photo-1666205450682-2c78a4929a5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDg1fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
                         alt="Image"
-                        class="img-fluid"
+                        className="img-fluid"
                       />
-                      <div class="contents">
+                      <div className="contents">
                         <h3>Card Vol. 3</h3>
-                        <div class="cat">Mockup</div>
+                        <div className="cat">Mockup</div>
                       </div>
                     </a>
                   </div>
